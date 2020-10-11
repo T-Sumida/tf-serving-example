3505e90cb6db8ae64684eac984915fbd34571f66
T-Sumida <ui.suto05@gmail.com>
2020-10-11 18:34:26 +0900

    [add] docker start bash

---
+++ docker_start.sh
@@ -0,0 +1,11 @@
+# docker image を用意
+docker pull tensorflow/serving
+
+MODEL_NAME="tf-serving"
+EXPORT_DIR="$(pwd)/$MODEL_NAME"
+echo $EXPORT_DIR
+
+
+docker run -p 8501:8501 \
+--mount type=bind,source=$EXPORT_DIR,target=/models/$MODEL_NAME \
+-e MODEL_NAME=$MODEL_NAME -t tensorflow/serving --model_config_file_poll_wait_seconds=60
